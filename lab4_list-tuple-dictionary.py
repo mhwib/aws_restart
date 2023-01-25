a00d65cff20a1ffa57ddefe121a5b457171808e9
Mahanani Hadi Wibowo <80585377+mhwib@users.noreply.github.com>
2023-01-25 20:06:28 +0700

    Add py & csv files via upload

---
+++ lab4_list-tuple-dictionary.py
@@ -0,0 +1,29 @@
+# List
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "Orange"
+print(myFruitList)
+
+# Tuple
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Dictionary
+myFavoriteFruitDictionary = {
+    "Mahanani": "apple",
+    "Hadi": "banana",
+    "Wibowo": "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Mahanani"])
+print(myFavoriteFruitDictionary["Hadi"])
+print(myFavoriteFruitDictionary["Wibowo"])
